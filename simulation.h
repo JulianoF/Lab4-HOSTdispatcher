fa17b2dae68cadb6285bbc3a76c960903c157374
jasonmzx <jasonmvocet@gmail.com>
2024-03-01 23:31:33 -0500

    defining some structs and was just reading the requirements

---
+++ simulation.h
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+//TODO: Idk if we need this ExecSim struct tbh
+//From Lab 4 doc: 2)
+//* Process ID, priority, processor time remaining (in seconds), memory location. block size, and resources requested
+
+typedef struct {
+    
+    int pid;
+
+    //* Both of these in seconds
+    int allocated_exec_time; //Burst Execution time Required 
+    int remaining_exec_time; //Time Left for Exec 
+
+    int priority;
+    int memory_loc;
+    int block_size;
+
+    char* request_rsrc; //? This is a char buffer, as it might be an array... we'll see
+
+} ExecSimulation;  //Execution of process Simulation
+
+
+typedef struct {
+
+    int* printers;
+    int* scanners;
+    int* modems;
+    int* CDs;
+
+} IOSim;
+
+
+//<arrival time>, <priority>, <processor time>, <Mbytes>, <#printers>, <#scanners>, <#modems>, <#CDs>
+
+typedef struct {
+
+    int arrival_time;
+    int priortiy;
+
+    int allocated_exec_time; //Processor Time it wants (In seconds)
+    int proc_size_in_mem; // In MB
+
+    IOSim io;
+
+} ProcSimulation; //Process Simulation
